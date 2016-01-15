44ca7f883a9f941c00d6da9cc1382d658e2e326a
Steve <stevatkov@gmail.com>
2016-01-14 20:30:10 -0500

    Added Unity Project

---
+++ Blink/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	private float dx;
+	private float dy;
+	public float speed;
+	private Rigidbody2D rb;
+	private 
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dx = Input.GetAxis("Horizontal");
+		dy = Input.GetAxis("Vertical");
+		rb.velocity = new Vector2(dx * speed,dy * speed);
+		var mousePos = Input.mousePosition;
+		mousePos.z = 10;
+		if (Input.GetMouseButtonDown(0)){
+			transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+		}
+	}
+}
