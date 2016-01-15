64bc5d2d47d7d5e48102f4900a5d632f78be8d9c
Tim McIlveen <tim.mcilveen@gmail.com>
2016-01-15 14:44:38 -0500

    Destroy With Blinks
    really crude now, still need to tweak like mad

---
+++ Blink/Assets/Explode.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explode : MonoBehaviour {
+
+	public GameObject enemy;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			Destroy (enemy);
+		}
+	}
+}
