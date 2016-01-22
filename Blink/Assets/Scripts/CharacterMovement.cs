cccc3b49c0abdbf086896953377819ecd8378444
Steve <stevatkov@gmail.com>
2016-01-22 15:16:21 -0500

    Idle, Walk, and Flip
    Added animator for transitioning between idle and walk. Also made the
    sprite flip to face the correct direction

--- Blink/Assets/Scripts/CharacterMovement.cs
+++ Blink/Assets/Scripts/CharacterMovement.cs
@@ -3,16 +3,20 @@ using System.Collections;
 
 public class CharacterMovement : MonoBehaviour {
 
+	[HideInInspector] public bool facingRight = true;
 	private float dx;
 	private float dy;
+	private float dxy;
 	public float speed;
 	private Rigidbody2D rb;
 	private 
 
+	Animator animator;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
-
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -20,10 +24,31 @@ public class CharacterMovement : MonoBehaviour {
 		dx = Input.GetAxis("Horizontal");
 		dy = Input.GetAxis("Vertical");
 		rb.velocity = new Vector2(dx * speed,dy * speed);
+
+		dxy = dx + dy;
+		//animator.SetFloat("MoveX",dx);
+		//animator.SetFloat("MoveY",dy);
+		animator.SetFloat("Move",dxy);
+
 		var mousePos = Input.mousePosition;
 		mousePos.z = 10;
 		if (Input.GetMouseButtonUp(0)){
 			transform.position = Camera.main.ScreenToWorldPoint(mousePos);
 		}
+
+		//Flips it according to value of dx
+		if (dx < 0 && !facingRight)
+			Flip();
+		else if (dx > 0 && facingRight)
+			Flip();
+	}
+
+	//Handles flipping of sprite
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
 	}
 }
