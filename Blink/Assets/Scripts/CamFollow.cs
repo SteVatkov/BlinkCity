8c35e0be6639e9624619ae95db2b37b740a6a66d
Tim McIlveen <tim.mcilveen@gmail.com>
2016-01-25 14:59:58 -0500

    Speed Adjustments
    changes to movespeed just committing to be able to pull properly or
    something I still don't know anything life is an illusion anyways

--- Blink/Assets/Scripts/CamFollow.cs
+++ Blink/Assets/Scripts/CamFollow.cs
@@ -8,7 +8,7 @@ public class CamFollow : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		transform.position = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z);
+		//transform.position = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z);
 	}
 
 	// Update is called once per frame
