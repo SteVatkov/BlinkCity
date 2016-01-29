f5ac30192b8eb0c151bcbb582be7886e39bd02ba
Steve <stevatkov@gmail.com>
2016-01-29 17:03:22 -0500

    Dumpy Walk Animation and simple enemy movement
    Enemy will move towards the player when the player is less than a
    certain amount of units away. It'll then switch from idle to walk
    animation. Will update this so it flips the sprite to face the correct
    direction it's headed in.

---
+++ Blink/Assets/Scripts/Enemy/SimpleMoveTowards.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMoveTowards : MonoBehaviour {
+
+	private Vector2 Position1;
+	private bool IsWalking = false;
+	public GameObject target;
+	public float speed;
+	public float DistanceFromTarget;
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+
+	void Update () 
+	{
+		Position1 = target.transform.position;
+		if(Vector2.Distance(transform.position, Position1) < DistanceFromTarget){
+			IsWalking = true;
+			transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), Position1, speed * Time.deltaTime);
+
+		}
+		else IsWalking = false;
+		animator.SetBool ("Walking", IsWalking);
+	}
+}
---
+++ Blink/Assets/Scripts/Player/UpdateOrder.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(SpriteRenderer))]
+public class UpdateOrder : MonoBehaviour {
+
+	private SpriteRenderer cachedSpriteRenderer;
+
+	private void Start () {
+		cachedSpriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+
+	private void Update () {
+		cachedSpriteRenderer.sortingOrder = -Mathf.RoundToInt(transform.position.y * 100);
+	}
+}
