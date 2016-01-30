f5a5bcb1cc6796c1bfbf19225de5b2a4b31ce3f9
Steve <stevatkov@gmail.com>
2016-01-30 18:16:00 -0500

    Enemy flip sprite
    Dumpy now flips according to the direction it's facing

--- Blink/Assets/Scripts/Enemy/SimpleMoveTowards.cs
+++ Blink/Assets/Scripts/Enemy/SimpleMoveTowards.cs
@@ -3,11 +3,15 @@ using System.Collections;
 
 public class SimpleMoveTowards : MonoBehaviour {
 
+	[HideInInspector] public bool facingRight = true;
 	private Vector2 Position1;
 	private bool IsWalking = false;
 	public GameObject target;
 	public float speed;
 	public float DistanceFromTarget;
+
+	private Vector3 prevloc = Vector2.zero;
+	private Vector3 curVel;
 
 	Animator animator;
 
@@ -27,5 +31,25 @@ public class SimpleMoveTowards : MonoBehaviour {
 		}
 		else IsWalking = false;
 		animator.SetBool ("Walking", IsWalking);
+	
+		//Checks previous location with current location to see which direction it's headed in
+		curVel = (transform.position - prevloc) / Time.deltaTime;
+		prevloc = transform.position;
+
+		//Flips it according to x value of curVel
+		if (curVel.x > 0 && facingRight)
+			Flip();
+		else if (curVel.x < 0 && !facingRight)
+			Flip();
 	}
+
+	//Handles flipping of sprite
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
 }
